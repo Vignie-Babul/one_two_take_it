48be423d40ac23cae0baa61eef7409c9097bf853
T1kh0n <tihonovkirill992@gmail.com>
2025-12-06 17:33:03 +0200

    Fixed utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,12 @@ import os
 
 
 def is_file_valid(path: str) -> bool:
-    if not os.path.exists(path):
-        print(f"Файл не найден: {file_path}")
-        return False
-    
-    if not os.path.isfile(path):
-        print(f"Указанный путь является директорией: {file_path}")
-        return False
-    
-    return True
+	if not os.path.exists(path):
+		print(f"File not found: {file_path}")
+		return False
+	
+	if not os.path.isfile(path):
+		print(f"This is a directory: {file_path}")
+		return False
+	
+	return True
