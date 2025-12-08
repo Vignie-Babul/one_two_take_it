73ce8c19bb8f2c29b22640b3fd6cc4148a824dbe
T1kh0n <tihonovkirill992@gmail.com>
2025-12-09 01:09:28 +0200

    Added platform.py

---
+++ src/platform.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import pygame
+
+from .physic import PhysicsBody
+
+
+class Platform(PhysicsBody):
+	def __init__(
+		self,
+		physics_world,
+		position: tuple[int | float, int | float],
+		size: tuple[int | float, int | float],
+		color: str = '#404040'
+	) -> None:
+		super().__init__(
+			physics_world=physics_world,
+			position=position,
+			size=size,
+			body_type='static',
+			shape_type='box',
+			friction=0.5
+		)
+
+		self._color = color
+		self._render()
+
+	def _render(self) -> None:
+		self.image.fill(self._color)
+
+		pygame.draw.rect(
+			self.image,
+			'#606060',
+			(0, 0, self.size[0], self.size[1]),
+			2
+		)
